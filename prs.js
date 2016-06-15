71c8149ec4e1fd0a0363a07c690178c003b88127
Alejandro Batista <abatista@vistaprint.com>
2016-06-15 14:51:27 -0400

    Showing up to 4 products

--- prs.js
+++ prs.js
@@ -4,9 +4,13 @@ $(document).ready(function() {
         url : 'http://vbu-channeldata-prs-service-dev.eu-west-1.elasticbeanstalk.com/v0/recommendation/en-us_rchp/' + utag_data.VisitorId,
         success : function(data){
             if (data != null && data.recommendations != null && data.recommendations.length > 0){
+                var count = 0;
                 data.recommendations.forEach(function(element, index, array){
-                    var pfid = element.productId;
-                    $('#product_' + pfid).show();
+                    if (count<4){
+                        var pfid = element.productId;
+                        $('#product_' + pfid).removeClass("hidden");
+                        count++;
+                    }
                 });
             }
         }
