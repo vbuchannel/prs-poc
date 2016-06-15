8cc4447179e80528f62e6af317911f553eb164ac
Alejandro Batista <abatista@vistaprint.com>
2016-06-15 13:29:47 -0400

    added product processing

--- prs.js
+++ prs.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
-    alert("hello");
 
     $.ajax({
-        url : 'http://vbu-channeldata-prs-service-dev.eu-west-1.elasticbeanstalk.com/v0/recommendation/en-us_rchp/103213',
-        success : function(){
-            alert('success');
+        url : 'http://vbu-channeldata-prs-service-dev.eu-west-1.elasticbeanstalk.com/v0/recommendation/en-us_rchp/' + utag_data.VisitorId,
+        success : function(data){
+            if (data != null && data.recommendations != null && data.recommendations.length > 0){
+                data.recommendations.forEach(function(element, index, array){
+                    var pfid = element.productId;
+                    $('#product_' + pfid).show();
+                });
+            }
         }
     });
 });
