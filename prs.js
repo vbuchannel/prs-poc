4df14743dd43e34900bd6ac3fd01b01945d05edd
Alejandro Batista <abatista@vistaprint.com>
2016-06-15 09:46:10 -0400

    added ajax request

--- prs.js
+++ prs.js
@@ -1,3 +1,10 @@
-$(document).ready({
-    alert("hello")
+$(document).ready(function() {
+    alert("hello");
+
+    $.ajax({
+        url : 'http://vbu-channeldata-prs-service-dev.eu-west-1.elasticbeanstalk.com/v0/recommendation/en-us_rchp/103213',
+        success : function(){
+            alert('success');
+        }
+    });
 });
