c3cc1447ace41594caf1396ab9ec884228d671b0
Alejandro Batista <abatista@vistaprint.com>
2016-06-16 14:40:49 -0400

    refactored the code to js standards

--- prs.js
+++ prs.js
@@ -1,20 +1,22 @@
 $(document).ready(function() {
-
+    MAX_SHOWN_PRODUCTS = 4;
+    PRS_URL_RCHP = 'http://vbu-channeldata-prs-service-dev.eu-west-1.elasticbeanstalk.com/v1/recommendation/en-us_rchp/';
     $.ajax({
-        url : 'http://vbu-channeldata-prs-service-dev.eu-west-1.elasticbeanstalk.com/v1/recommendation/en-us_rchp/' + utag_data.VisitorId,
+        url : PRS_URL_RCHP + utag_data.VisitorId,
         success : function(data){
             if (data != null && data.recommendations != null && data.recommendations.length > 0){
                 var count = 0;
                 var lastElement = $("first-element");
                 data.recommendations.forEach(function(element, index, array){
-                    if (count<4){
+                    if (count < MAX_SHOWN_PRODUCTS ){
                         var currentElement = $('#product_' + element.pfid);
                         currentElement.insertAfter(lastElement);
                         currentElement.removeClass("hidden");
-                        count++;
                         lastElement = currentElement;
+                        count++;
                     }
                 });
+
             }
         }
     });
