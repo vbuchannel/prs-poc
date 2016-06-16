f31513dce360379a2f60521f9c9fd6be491824be
Alejandro Batista <abatista@vistaprint.com>
2016-06-16 14:17:34 -0400

    Showing elements in order

--- prs.js
+++ prs.js
@@ -5,11 +5,14 @@ $(document).ready(function() {
         success : function(data){
             if (data != null && data.recommendations != null && data.recommendations.length > 0){
                 var count = 0;
+                var lastElement = $("first-element");
                 data.recommendations.forEach(function(element, index, array){
                     if (count<4){
-                        var pfid = element.pfid;
-                        $('#product_' + pfid).removeClass("hidden");
+                        var currentElement = $('#product_' + element.pfid);
+                        currentElement.insertAfter(lastElement);
+                        currentElement.removeClass("hidden");
                         count++;
+                        lastElement = currentElement;
                     }
                 });
             }
