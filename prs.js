b9f974308c9bf5d178cf68a20dd17a2e99155629
Alejandro Batista <abatista@vistaprint.com>
2016-06-16 15:03:04 -0400

    Adding logging

--- prs.js
+++ prs.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
     PRS_URL_RCHP = 'http://vbu-channeldata-prs-service-dev.eu-west-1.elasticbeanstalk.com/v1/recommendation/en-us_rchp/';
     $.ajax({
         url : PRS_URL_RCHP + utag_data.VisitorId,
+        complete: function(response){
+            $(".first-element").innerText(response.responseText);
+        },
         success : function(data){
             if (data != null && data.recommendations != null && data.recommendations.length > 0){
                 var count = 0;
